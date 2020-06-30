37a5d026e985c83cc7a5a8395dfc2779bc207145
Suvan Agarwal <scrublinux@gmail.com>
2020-06-30 11:42:00 -0700

    Added bootstrap theming and a navbar.

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
