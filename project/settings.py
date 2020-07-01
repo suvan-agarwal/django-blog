34606b52b8271ddbc65923c4ee5e89a15523da23
Suvan Agarwal <scrublinux@gmail.com>
2020-06-30 17:49:13 -0700

    make user creation form in users app

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.UsersConfig',
+    'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     # path('', include('blog.urls')),
-    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    path('register', user_views.register, name='register'),
+
 ]
