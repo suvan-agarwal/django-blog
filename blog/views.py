25fa5ce6bf923b0ee9cd1bb45e7ece01f8c76a77
Suvan Agarwal <scrublinux@gmail.com>
2020-06-30 17:11:00 -0700

    Add posts model

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'scrublinux',
-        'title': 'first blog post',
-        'content': 'content of first post',
-        'date_posted': 'June 28 2020',
-    },
-    {
-        'author': 'jane doe',
-        'title': '2nd blog post',
-        'content': 'content of 2nd post',
-        'date_posted': 'June 29 2020',
-    },
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
